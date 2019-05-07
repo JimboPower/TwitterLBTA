494e6dfa7226b1713d19d3b6a610965230cb8323
JimboPower <edoardo.decal@gmail.com>
2019-05-07 18:34:37 +0200

    added collectionView Header

--- TwitterLBTA/TwitterLBTA/HomeController.swift
+++ TwitterLBTA/TwitterLBTA/HomeController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 
+class WorldCell: UICollectionViewCell {
+    
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Edo Edo Edo"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    func setUpViews() {
+        backgroundColor = .yellow
+        addSubview(wordLabel)
+        wordLabel.translatesAutoresizingMaskIntoConstraints = false
+        wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
 class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let cellId = "cellId"
+    let headerId = "headerId"
     
     override func viewDidLoad() {
             super.viewDidLoad()
         collectionView?.backgroundColor = .green
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.register(WorldCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -24,7 +54,6 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .blue
         return cell
     }
 
@@ -32,5 +61,15 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         return CGSize(width: view.frame.width, height: 50)
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        header.backgroundColor = .blue
+        return header
+    }
     
 }
